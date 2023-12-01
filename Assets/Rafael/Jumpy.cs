04fcc2ab11415cafebfb7253d492c8d45123b9f1
Gefter <1713331@mukilteo.wednet.edu>
2023-12-01 09:32:39 -0800

    Started work on jumping and stuff

--- Assets/Rafael/Jumpy.cs
+++ Assets/Rafael/Jumpy.cs
@@ -9,13 +9,33 @@ public class Jumpy : MonoBehaviour
     public float Speed = 2f;
     public float jump = 10f;
     public float Gravity = -9.8f;
+    public bool isGrounded;
 
+    float m_MaxDistance;
 
+    bool m_HitDetect;
+
+    Collider m_collider;
+    RaycastHit m_Hit;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
+    }
+
+    private void FixedUpdate()
+    {
+
+        m_HitDetect = Physics.BoxCast(m_collider.bounds.center, transform.localScale, transform.forward, out m_Hit, transform.rotation, m_MaxDistance);
+
+        if (m_HitDetect)
+        {
+
+            Debug.Log("Hit" + m_Hit.collider.name);
+
+        }
+
     }
 
     // Update is called once per frame
@@ -27,7 +47,9 @@ public class Jumpy : MonoBehaviour
         characterController.Move(move * Speed * Time.deltaTime);
 
 
-        if(Input.GetButtonDown("Jump") && transform.position.y< -0.5f)
+        
+
+        if(Input.GetButtonDown("Jump") && transform.position.y< 0.12f)
         {
 
             velocity.y = jump;
