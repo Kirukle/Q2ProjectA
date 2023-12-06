03be24872fad37f6eb7b5a799e41e60e372ceb41
MUKILTEO\1713357 <Kolbscash@gmail.com>
2023-12-06 10:23:24 -0800

    scan mechanics wip

---
+++ Assets/Kaycee/Scanable.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scanable : MonoBehaviour
+{
+    public bool activated;
+    public GameObject textPrefab;
+    public float scanTime;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (activated == true)
+        {
+
+        }
+    }
+}
