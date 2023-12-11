267168dd8136134fddf07623fd151ccd52f80650
MUKILTEO\1713357 <Kolbscash@gmail.com>
2023-12-11 08:13:19 -0800

    set up tmp scanning finished

--- Assets/Kaycee/Scanable.cs
+++ Assets/Kaycee/Scanable.cs
@@ -7,13 +7,14 @@ public class Scanable : MonoBehaviour
     public bool activated;
     public GameObject textPrefab;
     public float scanTime;
+    public string TextName;
 
     // Update is called once per frame
     void Update()
     {
         if (activated == true)
         {
-            Instantiate(textPrefab);
+            Instantiate(textPrefab).GetComponent<TMPro.TMP_Text>().SetText(TextName);
         }
     }
 }
