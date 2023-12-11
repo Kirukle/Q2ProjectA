c95d737d69625085ff0bcdb88145b814da607e49
Xavier-Ford <XavierFord1040@gmail.com>
2023-12-10 20:54:06 -0800

    Bugs in your skin!!!!
    Bugs in your skin!!!!
    Bugs in your skin!!!!
    Bugs in your skin!!!!
    Bugs in your skin!!!!
    Bugs in your skin!!!!
    Bugs in your skin!!!!
    Bugs in your skin!!!!
    Bugs in your skin!!!!
    Bugs in your skin!!!!
    Bugs in your skin!!!!
    Bugs in your skin!!!!
    Bugs in your skin!!!!
    Bugs in your skin!!!!
    Bugs in your skin!!!!
    Bugs in your skin!!!!
    Bugs in your skin!!!!
    Bugs in your skin!!!!
    Bugs in your skin!!!!
    Bugs in your skin!!!!
    Bugs in your skin!!!!
    Bugs in your skin!!!!

--- Assets/Xavier/Scripts/Interactables.cs
+++ Assets/Xavier/Scripts/Interactables.cs
@@ -1,12 +1,26 @@
 using UnityEngine;
 using UnityEngine.Events;
 
-public class Interactable : MonoBehaviour
+public class Interactables : MonoBehaviour
 {
     public UnityEvent onScan;
+    public UnityEvent<bool> canMoveChanged = new UnityEvent<bool>();
+    public UnityEvent<bool> canSprintChanged = new UnityEvent<bool>();
+    public UnityEvent<bool> canJumpChanged = new UnityEvent<bool>();
 
-    public void Scan()
+    public void OnScanned()
     {
         onScan.Invoke();
+
+        ThirdMover thirdMover = GetComponent<ThirdMover>();
+        Jumpy jumpy = GetComponent<Jumpy>();
+
+        bool canMoveValue = thirdMover.CanMove;
+        bool canSprintValue = thirdMover.CanSprint;
+        bool canJumpValue = jumpy.canJump;
+
+        canMoveChanged.Invoke(canMoveValue);
+        canSprintChanged.Invoke(canSprintValue);
+        canJumpChanged.Invoke(canJumpValue);
     }
 }
