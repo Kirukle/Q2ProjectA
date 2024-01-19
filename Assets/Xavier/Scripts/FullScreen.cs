54111b01bce06460f4d06ca38b3f5faaa52f6372
MUKILTEO\1714014 <1714014@mukilteo.wednet.edu>
2024-01-19 09:03:44 -0800

    eh

--- Assets/Xavier/Scripts/FullScreen.cs
+++ Assets/Xavier/Scripts/FullScreen.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MainCamera : MonoBehaviour
+public class FullScreen : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Screen.SetResolution(1920, 1080, FullScreenMode.ExclusiveFullScreen);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKeyDown(KeyCode.F11))
+        {
+            Screen.fullScreen = !Screen.fullScreen; // full screen toggle
+        }
     }
 }
--- Assets/Xavier/Scripts/ResolutionManager.cs
+++ Assets/Xavier/Scripts/ResolutionManager.cs
@@ -2,49 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using System.Linq;
 
 public class ResolutionManager : MonoBehaviour
 {
     [SerializeField] private TMP_Dropdown resolutionDropdown;
 
     private Resolution[] resolutions;
-    private List<Resolution> filteredResolutions;
-
-    private float currentRefreshRate;
     private int currentResolutionIndex = 0;
 
     void Start()
     {
         resolutions = Screen.resolutions;
-        filteredResolutions = new List<Resolution>();
         resolutionDropdown.ClearOptions();
-        currentRefreshRate = Screen.currentResolution.refreshRate;
-
-        Debug.Log("RefreshRate: " + currentRefreshRate);
-
-        for (int i = 0; i < resolutions.Length; i++)
-        {
-            if (resolutions[i].refreshRate == currentRefreshRate)
-            {
-                filteredResolutions.Add(resolutions[i]);
-            }
-        }
-
-        for (int i = 0; i < resolutions.Length; i++)
-        {
-            if (!filteredResolutions.Any(x => x.width == resolutions[i].width && x.height == resolutions[i].height))
-            {
-                filteredResolutions.Add(resolutions[i]);
-            }
-        }
 
         List<string> options = new List<string>();
-        for (int i = 0; i < filteredResolutions.Count; i++)
+        for (int i = 0; i < resolutions.Length; i++)
         {
-            string resolutionOption = filteredResolutions[i].width + "x" + filteredResolutions[i].height;
+            string resolutionOption = resolutions[i].width + "x" + resolutions[i].height;
             options.Add(resolutionOption);
-            if (filteredResolutions[i].width == Screen.width && filteredResolutions[i].height == Screen.height)
+            if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
             {
                 currentResolutionIndex = i;
             }
@@ -61,7 +37,7 @@ public class ResolutionManager : MonoBehaviour
 
     public void SetResolution(int resolutionIndex)
     {
-        Resolution resolution = filteredResolutions[resolutionIndex];
+        Resolution resolution = resolutions[resolutionIndex];
         Screen.SetResolution(resolution.width, resolution.height, true);
     }
 }
