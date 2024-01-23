4e5d5a21e5db60c5580530b3e7f9bf83e270a3df
Xavier-Ford <XavierFord1040@gmail.com>
2024-01-22 22:49:58 -0800

    Fixed resolution settings
    Internally crying, Internally crying, Internally crying, Internally crying, Internally crying, Internally crying, Internally crying, Internally crying, Internally crying, Internally crying, Internally crying, Internally crying, Internally crying, Internally crying, Internally crying, Internally crying, Internally crying, Internally crying,

--- Assets/Xavier/Scripts/Resolutions.cs
+++ Assets/Xavier/Scripts/Resolutions.cs
@@ -5,29 +5,27 @@ using System.Linq;
 using UnityEngine;
 
 public class Resolutions : MonoBehaviour
-// https://www.youtube.com/watch?v=9olg-0b8mEk
 {
-    [SerializeField] private TMPro.TMP_Dropdown resolutionDropDown;    //check name!
+    [SerializeField] private TMPro.TMP_Dropdown resolutionDropDown;
 
     private Resolution[] resolutions;
     private List<Resolution> filteredResolutions;
 
-    //private float currentRefreshRate;
     private int currentResolutionIndex = 0;
     [HideInInspector] public Resolution resolution;
+
     private void Start()
     {
         resolutions = Screen.resolutions;
         filteredResolutions = new List<Resolution>();
 
         resolutionDropDown.ClearOptions();
-        //currentRefreshRate = Screen.currentResolution.refreshRate;
 
-        for (int i = 0; i < resolutions.Length; i++)
+        foreach (var res in resolutions)
         {
-            if (!filteredResolutions.Any(x => x.width == resolutions[i].width && x.height == resolutions[i].height))  //check if resolution already exists in list
+            if (!filteredResolutions.Any(x => x.width == res.width && x.height == res.height))
             {
-                filteredResolutions.Add(resolutions[i]);  //add resolution to list if it doesn't exist yet
+                filteredResolutions.Add(res);
             }
         }
 
@@ -50,24 +48,22 @@ public class Resolutions : MonoBehaviour
     public void SetResolution(int resolutionIndex)
     {
         resolution = filteredResolutions[resolutionIndex];
+        
+        // Debug.Log("Changing resolution to: " + resolution.width + " x " + resolution.height);
+
         if (PlayerPrefs.GetInt("ScreenMode") == 0)
         {
-            // Switch to 4k full
             Screen.SetResolution(resolution.width, resolution.height, true);
-
-            //Screen.fullScreen = true;
             Screen.fullScreenMode = FullScreenMode.FullScreenWindow;
         }
-        if (PlayerPrefs.GetInt("ScreenMode") == 1)
+        else if (PlayerPrefs.GetInt("ScreenMode") == 1)
         {
             Screen.SetResolution(resolution.width, resolution.height, true);
-            //Screen.fullScreen = true;
             Screen.fullScreenMode = FullScreenMode.ExclusiveFullScreen;
         }
-        if (PlayerPrefs.GetInt("ScreenMode") == 2)
+        else if (PlayerPrefs.GetInt("ScreenMode") == 2)
         {
             Screen.SetResolution(resolution.width, resolution.height, false);
-            //Screen.fullScreen = false;
             Screen.fullScreenMode = FullScreenMode.Windowed;
         }
     }
