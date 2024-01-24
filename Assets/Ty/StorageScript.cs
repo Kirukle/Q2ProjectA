2dea5d11475bd5715e319e49e7038d67600f25ce
Kirukle <113458461+Kirukle@users.noreply.github.com>
2024-01-23 20:58:17 -0800

    Bunch of Changes

--- Assets/Ty/StorageScript.cs
+++ Assets/Ty/StorageScript.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class StorageScript : MonoBehaviour
 {
@@ -11,6 +13,20 @@ public class StorageScript : MonoBehaviour
         if (Collectors > 3)
         {
             Debug.Log("Completed");
+            SceneManager.LoadScene("EndingScene");
         }
+    }
+    void OnGUI()
+    {
+        int w = Screen.width, h = Screen.height;
+
+        GUIStyle style = new GUIStyle();
+
+        Rect rect = new Rect(0, 0, w, h * 4 / 100);
+        style.alignment = TextAnchor.LowerCenter;
+        style.fontSize = h * 4 / 100;
+        style.normal.textColor = new Color(0f, 0f, 0.5f, 1.0f);
+        string text = "MISSION OPERATION :  " + Collectors.ToString() + "/4";
+        GUI.Label(rect, text, style);
     }
 }
